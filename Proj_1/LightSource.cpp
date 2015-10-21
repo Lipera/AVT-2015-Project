bc1cfb141e40413eb823cfb517b216bbd2d67a09
João Pedro Pereira <joao.pedro.pereira@tecnico.ulisboa.pt>
2015-10-21 16:22:37 +0100

    Update LightSource.cpp
    Esquelto do LightSource

---
+++ Proj_1/LightSource.cpp
@@ -0,0 +1,53 @@
+#include LightSource.h
+
+LightSource::LightSource(GLenum number) {
+
+}
+
+LightSource::~LightSource() {
+
+}
+
+void LightSource::setAmbient(Vector4 ambient) {
+
+}
+
+void LightSource::setDiffuse(Vector4 diffuse) {
+
+}
+
+void LightSource::setSpecular(Vector4 specular) {
+
+}
+
+void LightSource::setPosition(Vector4 position) {
+
+}
+
+void LightSource::setDirection(Vector3 direction) {
+
+}
+
+void LightSource::setCutOff(double cut_off) {
+
+}
+
+void LightSource::setExponent(double exponent) {
+
+}
+
+GLenum LightSource::getNum() {
+
+}
+
+bool LightSource::getState() {
+
+}
+
+bool LightSource::setState(bool state) {
+
+}
+
+void LightSource::draw() {
+
+}
